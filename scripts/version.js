e87166e7ab254e78648f7ce36c4b1cb3940048b5
Karthik Balasubramanian <kartbala@gmail.com>
2025-05-15 17:52:01 -0400

    Increase Dots information size to match letter-position in celebration screen

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "567fae2",
-  date: "2025-05-15T21:51:09Z",
-  timestamp: 1747345869000
+  commit: "096add9",
+  date: "2025-05-15T21:52:01Z",
+  timestamp: 1747345921000
 };
