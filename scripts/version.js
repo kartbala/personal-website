df26edbe4d6e6409f73e75f8ac8b3a706cad69fe
Karthik Balasubramanian <kartbala@gmail.com>
2025-05-15 17:14:05 -0400

    Remove navigation bar for distraction-free experience

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "e0bc36f",
-  date: "2025-05-15T21:12:13Z",
-  timestamp: 1747343533000
+  commit: "c543e1b",
+  date: "2025-05-15T21:14:05Z",
+  timestamp: 1747343645000
 };
