aa49fdd6ed7d1bdd689b6636031a7990089ed065
Karthik Balasubramanian <kartbala@gmail.com>
2025-05-15 17:17:39 -0400

    Add minimal UI option to hide all UI elements except braille dots

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "c543e1b",
-  date: "2025-05-15T21:14:05Z",
-  timestamp: 1747343645000
+  commit: "df26edb",
+  date: "2025-05-15T21:17:39Z",
+  timestamp: 1747343859000
 };
