ad18513dc8b2e2b69df484975e09235932f4c8aa
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-06-05 18:49:54 -0400

    Add interactive life expectancy visualization

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "88f0d91",
-  date: "2025-05-23T17:54:15Z",
-  timestamp: 1748022855000
+  commit: "0ffe395",
+  date: "2025-06-05T22:47:43Z",
+  timestamp: 1749163663000
 };
