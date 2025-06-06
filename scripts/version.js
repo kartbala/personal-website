749b29cddde613e7acf8c0993ec613fb18410a75
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-06-06 11:18:48 -0400

    Simplify links page and add new sections

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "f5d59d5",
-  date: "2025-06-06T14:17:16Z",
-  timestamp: 1749219436000
+  commit: "350e6ea",
+  date: "2025-06-06T15:17:32Z",
+  timestamp: 1749223052000
 };
