082d340aa78a799dc20fcf9d3d73170fe97deb51
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-06-10 17:05:56 -0400

    Remove GDP demos and add external links page

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "4e2f387",
-  date: "2025-06-09T22:03:58Z",
-  timestamp: 1749506638000
+  commit: "436f6c7",
+  date: "2025-06-10T21:01:51Z",
+  timestamp: 1749589311000
 };
