807bbb3512869ada27d0635afe57dcb9e1b6195f
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-06-11 17:13:15 -0400

    Enhance Time Tax page visuals and message

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "0e3d8a2",
-  date: "2025-06-11T20:58:38Z",
-  timestamp: 1749675518000
+  commit: "cb19bfb",
+  date: "2025-06-11T21:04:42Z",
+  timestamp: 1749675882000
 };
