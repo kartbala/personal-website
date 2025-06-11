3d317655dc05a12324dab088551176add51134a5
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-06-11 16:59:25 -0400

    Add car ownership step to time tax calculator

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "436f6c7",
-  date: "2025-06-10T21:01:51Z",
-  timestamp: 1749589311000
+  commit: "0e3d8a2",
+  date: "2025-06-11T20:58:38Z",
+  timestamp: 1749675518000
 };
