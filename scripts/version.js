2e97166f3263433fd41d75e73a7640446de00836
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-06-15 20:14:52 -0400

    Fix audio visualizers

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "aa4e7ee",
-  date: "2025-06-15T21:50:49Z",
-  timestamp: 1750024249000
+  commit: "451c992",
+  date: "2025-06-16T00:07:47Z",
+  timestamp: 1750032467000
 };
