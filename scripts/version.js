fe35c0f8d8f2e52b2e3e3d3ba1b00fcfe240e246
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-06-23 14:35:29 -0400

    Document search, automation, and demos

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "017c011",
-  date: "2025-06-22T17:11:36Z",
-  timestamp: 1750612296000
+  commit: "a3e4924",
+  date: "2025-06-23T18:34:34Z",
+  timestamp: 1750703674000
 };
