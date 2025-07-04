1886a0799b4dde8c4ff163410a6965bbb0e65c9d
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2025-07-04 08:57:26 +0530

    Style trump bill page and highlight impacts

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 // This file is automatically updated by the pre-commit hook
 window.APP_VERSION = {
-  commit: "caa7f2b",
-  date: "2025-07-04T03:04:44Z",
-  timestamp: 1751598284000
+  commit: "708904a",
+  date: "2025-07-04T03:25:00Z",
+  timestamp: 1751599500000
 };
